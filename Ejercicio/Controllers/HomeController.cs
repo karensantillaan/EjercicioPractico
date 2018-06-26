0bbb9deacd37a6d985b2db6300b1ab59f07339c3
karensantillaan <karenm1104@gmail.com>
2018-06-26 02:06:15 -0700

    Initial commit

---
+++ Ejercicio/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using ClosedXML.Excel;
+using Ejercicio.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+//using Microsoft.Office.Interop.Excel;
+using System.Web.Http;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Ejercicio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CargarExcel(string path)
+        {
+            path = @"C:\Users\Karen\Downloads\Calificaciones.xlsx";
+            var file = ReadExcelFile("Sheet1", path);
+            var rows = file.Rows;
+
+            var datos = Funciones.ObtenerDatos(rows);
+
+            var calificacionMayor = Funciones.CalificacionMayor(datos);
+
+            var calificacionMenor = Funciones.CalificacionMenor(datos);
+
+            var promedioCalificaciones = Funciones.PromedioFinal(datos);
+
+            var promediosGrupos = Funciones.PromediosGrupos(datos);
+           
+            return View();            
+        }
+
+        private DataTable ReadExcelFile(string sheetName, string path)
+        {
+
+            using (OleDbConnection conn = new OleDbConnection())
+            {
+                DataTable dt = new DataTable();
+                string Import_FileName = path;
+                string fileExtension = Path.GetExtension(Import_FileName);
+                if (fileExtension == ".xls")
+                    conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Import_FileName + ";" + "Extended Properties='Excel 8.0;HDR=YES;'";
+                if (fileExtension == ".xlsx")
+                    conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Import_FileName + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+                using (OleDbCommand comm = new OleDbCommand())
+                {
+                    comm.CommandText = "Select * from [" + sheetName + "$]";
+
+                    comm.Connection = conn;
+
+                    using (OleDbDataAdapter da = new OleDbDataAdapter())
+                    {
+                        da.SelectCommand = comm;
+                        da.Fill(dt);
+                        return dt;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio/Models/Funciones.cs
@@ -0,0 +1,146 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio.Models
+{
+    public class Funciones
+    {
+        public static List<Calificaciones> ObtenerDatos(DataRowCollection rows)
+        {
+            List<Calificaciones> layoutList = new List<Calificaciones>();
+            for (int i = 0; i < rows.Count; i++)
+            {                
+                Calificaciones layout = new Calificaciones();
+                var element = rows[i].ItemArray;
+                //var nombres = element[0];
+                //var apellidoPaterno = element[1];
+                //var apellidoMaterno = element[2];
+                //var Grado = element[3];
+                //var Grupo = element[4];
+                //var Calificacion = element[5];
+
+                layout.Nombres = element[0].ToString();
+                layout.ApellidoPaterno = element[1].ToString();
+                layout.ApellidoMaterno = element[2].ToString();
+                layout.Grado = Convert.ToInt32(element[3]);
+                layout.Grupo = element[4].ToString();
+                layout.Calificacion = Convert.ToDecimal(element[5]);
+                    
+                layoutList.Add(layout);               
+                
+            }
+            return layoutList;
+        }
+
+        public static string CalificacionMayor(List<Calificaciones> lista)
+        {
+            var MejorAlumno = "";
+            decimal MaxCalificacion = 0; 
+           
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Calificacion > MaxCalificacion)
+                {
+                    MaxCalificacion = lista[i].Calificacion;
+                    MejorAlumno = lista[i].Nombres + " " + lista[i].ApellidoPaterno + " " + lista[i].ApellidoMaterno;
+                }
+            }
+
+            return MejorAlumno;
+        }
+
+        public static string CalificacionMenor(List<Calificaciones> lista)
+        {
+            var PeorAlumno = "";
+            decimal MaxCalificacion = 10; 
+           
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Calificacion < MaxCalificacion)
+                {
+                    MaxCalificacion = lista[i].Calificacion;
+                    PeorAlumno = lista[i].Nombres + " " + lista[i].ApellidoPaterno + " " + lista[i].ApellidoMaterno;
+                }
+            }
+
+            return PeorAlumno;
+        }
+
+        public static decimal PromedioFinal(List<Calificaciones> lista)
+        {
+            decimal promedio = 0;
+            decimal sumaCalif = 0;
+            int totalAlumnos = lista.Count(); 
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                sumaCalif += lista[i].Calificacion;
+            }
+
+            promedio = sumaCalif / totalAlumnos;
+
+            return promedio; 
+        }
+
+        public static List<Promedios> PromediosGrupos(List<Calificaciones> lista)
+        {
+            decimal sumaCalif1 = 0;
+            decimal sumaCalif2 = 0;
+            decimal sumaCalif3 = 0;
+            int total1 = 0;
+            int total2 = 0;
+            int total3 = 0;
+
+            List<Promedios> promediosGrupos = new List<Promedios>();
+            Promedios promedios = new Promedios();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                switch (lista[i].Grado)
+                {
+                    case 1:
+                        sumaCalif1 += lista[i].Calificacion;
+                        total1++;
+                    break;
+                    case 2:
+                        sumaCalif2 += lista[i].Calificacion;
+                        total2++;
+                    break;
+                    case 3:
+                        sumaCalif3 += lista[i].Calificacion;
+                        total3++;
+                    break;
+
+                }                
+            }
+
+            promedios.Promedio1 = sumaCalif1 / total1;
+            promedios.Promedio2 = sumaCalif2 / total2;
+            promedios.Promedio3 = sumaCalif3 / total3;
+            promediosGrupos.Add(promedios);
+
+            return promediosGrupos;
+        }
+
+        public class Calificaciones
+        {
+            public string ApellidoPaterno { get; set; }
+            public string ApellidoMaterno { get; set; }
+            public string Nombres { get; set; }
+            public int Grado { get; set; }
+            public string Grupo { get; set; }
+            public decimal Calificacion { get; set; }
+        }
+
+        public class Promedios
+        {
+            public decimal Promedio1 { get; set; }
+            public decimal Promedio2 { get; set; }
+            public decimal Promedio3 { get; set; }
+        }
+    }
+}
